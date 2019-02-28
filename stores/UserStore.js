05d2511e911569b6f77e51cfc0001b96cb9b8aaa
Sergejs0728 <sergejs.nescereckis0728@gmail.com>
2019-02-28 23:12:04 +1000

    fixed

--- stores/UserStore.js
+++ stores/UserStore.js
@@ -1,51 +1,51 @@
 /* @flow */
-import { observable, action, computed } from "mobx";
+import { observable, action, computed } from 'mobx';
 import { ListView, Text } from 'react-native';
 export default class UserStore {
-  // @observable firstName = "Prakash";
-  // @observable lastName = "Appleseed";
-  // @observable email = "send@appleseed.com";
-  // @observable phone = "1155667788";
+	// @observable firstName = "test";
+	// @observable lastName = "Appleseed";
+	// @observable email = "send@appleseed.com";
+	// @observable phone = "1155667788";
 
-  // @action data(data: Object) {
-  //   if (data.firstName) {
-  //     this.firstName = data.firstName;
-  //   }
-  //   if (data.lastName) {
-  //     this.lastName = data.lastName;
-  //   }
-  //   if (data.email) {
-  //     this.email = data.email;
-  //   }
-  //   if (data.phone) {
-  //     this.phone = data.phone;
-  //   }
-  // }
+	// @action data(data: Object) {
+	//   if (data.firstName) {
+	//     this.firstName = data.firstName;
+	//   }
+	//   if (data.lastName) {
+	//     this.lastName = data.lastName;
+	//   }
+	//   if (data.email) {
+	//     this.email = data.email;
+	//   }
+	//   if (data.phone) {
+	//     this.phone = data.phone;
+	//   }
+	// }
 
-  // @computed get fullName(): string {
-  //   return `${this.firstName} ${this.lastName}`;
-  // }
+	// @computed get fullName(): string {
+	//   return `${this.firstName} ${this.lastName}`;
+	// }
 
-  //  @observable list = [
-  //   { text: 'Hello World!' },
-  //   { text: 'Hello React Native!' },
-  //   { text: 'Hello MobX!' }
-  // ];
+	//  @observable list = [
+	//   { text: 'Hello World!' },
+	//   { text: 'Hello React Native!' },
+	//   { text: 'Hello MobX!' }
+	// ];
 
-  // ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-  // @computed get dataSource() { return this.ds.cloneWithRows(this.list); }
+	// ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+	// @computed get dataSource() { return this.ds.cloneWithRows(this.list); }
 
-  @observable all = [
-    { id: 1, name: 'Prakash', email: 'prakash.techdoodles@gmail.com'},
-    { id: 2, name: 'Kruti', email: 'kruti.techdoodles@gmail.com'},
-  ];
+	@observable all = [
+		{ id: 1, name: 'test1', email: 'test1.techdoodles@gmail.com' },
+		{ id: 2, name: 'test2', email: 'test2.techdoodles@gmail.com' },
+	];
 
-  @action add(data) {
-    const existing = this.all;
-    this.all = existing.concat(data);
-  }
+	@action add(data) {
+		const existing = this.all;
+		this.all = existing.concat(data);
+	}
 
-  @computed get all_contact(): string {
-    return `${this.all}`;
-  }
+	@computed get all_contact(): string {
+		return `${this.all}`;
+	}
 }
